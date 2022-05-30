2183b4b5fdfed64756cdf2e51a144e8c78ac7ce6
Shubham Kumar Verma <84174011+vermashubham6351@users.noreply.github.com>
2022-05-31 00:03:09 +0530

    Time: 36 ms (18.53%), Space: 19.5 MB (59.85%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-         unordered_map < int , int > umap;
-  for(int i =0 ; i<n ; i++)
-  umap[nums[i]]++;
+         unordered_map < int , int > umap;//create map in o(n)
+           for(int i =0 ; i<n ; i++)
+             umap[nums[i]]++;
   
-  for(auto x : umap)
-  if(x.second > n/2)
-  return x.first;
+           for(auto x : umap)            //in searching time o(n)
+            if(x.second > n/2)
+            return x.first;
         return 0;
         
     }
 };
+
+ //TC - O(n)  and  SC - O(n)
