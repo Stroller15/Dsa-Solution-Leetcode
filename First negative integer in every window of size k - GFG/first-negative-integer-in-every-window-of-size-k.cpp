c2e801434fe37ca6b754c52c46a67dc9d0401290
Shubham Kumar Verma <84174011+vermashubham6351@users.noreply.github.com>
2022-06-23 15:31:07 +0530

    Added solution - LeetHub

--- First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -29,53 +29,31 @@ int main() {
 
 
 vector<long long> printFirstNegativeInteger(long long int A[],
-                                             long long int N, long long int k) {
+                                             long long int N, long long int K) {
                                                                                            
-        // long long int i = 0 , j = 0;
-        // vector<long long> res;
-        // queue<long long> temp;
+        long long int i = 0 , j = 0;
+        vector<long long> res;
+        queue<long long> temp;
         
-        // while(j < N){
-        //     if(A[j] < 0)  temp.push(A[j]);
+        while(j < N){
+            if(A[j] < 0)  temp.push(A[j]);
             
-        //     if(j-i+1 < K)  j++;
+            if(j-i+1 < K)  j++;
             
-        //     else if(j-i+1 == K){
-        //         if(temp.empty()) res.push_back(0);
-        //         else{
-        //             res.push_back(temp.front());
-        //             if(A[i] == temp.front())
-        //             temp.pop();
-        //             i++;
-        //             j++;
-        //         }
-        //     }
-        // }
-        
-        // return res;
-        queue<long long> q;
-     vector<long long> ans;
-     int j=0,i=0;
-     while(j<N){
-         if(A[j]<0){
-             q.push(A[j]);
-         }
-         if(j-i+1<k){
-             j++;
-         }
-         else if(j-i+1==k){
-            if(q.size()==0)
-                ans.push_back(0);
-            else
-                ans.push_back(q.front());
+            else if(j-i+1 == K){
+                if(temp.size() == 0) res.push_back(0);
+                else
+                    res.push_back(temp.front());
+                    if(A[i] < 0)
+                    temp.pop();
+                    i++;
+                    j++;
                 
-            if(A[i]<0){
-                q.pop();
             }
-            i++;j++;
-         }
-     }
-     return ans;                                         
+        }
+        
+        return res;
+                                               
  
        
                        
