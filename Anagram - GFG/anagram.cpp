0051f7793e6d26e39ba4de9b15180c5cad0b8c57
Shubham Kumar Verma <84174011+vermashubham6351@users.noreply.github.com>
2022-07-11 12:44:13 +0530

    Added solution - LeetHub

--- Anagram - GFG/anagram.cpp
+++ Anagram - GFG/anagram.cpp
@@ -9,17 +9,52 @@ class Solution
     public:
     //Function is to check whether two strings are anagram of each other or not.
     bool isAnagram(string a, string b){
-        
-        // Your code here
         if(a.length() != b.length()){
             return false;
-        }else{
-            
-            sort(a.begin() , a.end());
-            sort(b.begin() , b.end());
-            if(a == b) return true;
         }
-        return false;
+        
+     unordered_map<char,int> mp;
+     for(int i = 0 ; i < b.length() ; i++){
+         mp[b[i]]++;
+     }
+     
+     int count = mp.size();
+     
+     for(int i = 0 ; i < a.length() ; i++){
+         if(mp.find(a[i]) !=mp.end()){
+             mp[a[i]]--;
+             if(mp[a[i]] == 0){
+                 count--;
+             }
+         }
+     }
+     
+    if(count == 0) return true;
+    else
+    return false;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
 };
