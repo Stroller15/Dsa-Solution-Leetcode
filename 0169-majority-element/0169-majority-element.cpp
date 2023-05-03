5f03e0b46e9cae581e145e9075200e4c8558262e
Shubham Verma <84174011+Stroller15@users.noreply.github.com>
2023-05-03 20:02:52 +0530

    Time: 30 ms (7.75%), Space: 19.4 MB (96.92%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,43 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+        
+//         unordered_map<int,int> umap;
+        
+//         for(auto num : nums) {
+//             umap[num]++;
+//         }
+        
+//         for(auto element : umap) {
+//             if(element.second > nums.size()/2)
+//                 return element.first;
+//         }
+        
+//         return 0;
+//     }
+// };
+//  Time Complexity - O(N)
+//  Space Complexity - O(N)
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+
+        int majority, count = 0;
         
-        unordered_map<int,int> umap;
-        
-        for(auto num : nums) {
-            umap[num]++;
+        for(int i = 0 ; i < nums.size() ; i++) {
+
+            if(count) {
+                count += (majority == nums[i] ? 1 : -1);
+            }
+
+            else {
+                majority = nums[i];
+                count = 1;
+            }
         }
-        
-        for(auto element : umap) {
-            if(element.second > nums.size()/2)
-                return element.first;
-        }
-        
-        return 0;
+        return majority;
     }
 };
+// Time complexity - O(N) 
+// Space complexity - O(1)
