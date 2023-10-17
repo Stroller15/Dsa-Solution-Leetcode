998ecda22d01d45690c4715d802cda18dd71e77e
Shubham Verma <84174011+Stroller15@users.noreply.github.com>
2023-10-17 15:50:02 +0530

    Time: 16 ms (20.48%), Space: 21.1 MB (8.54%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> maxL(n);
+        vector<int> maxR(n);
+
+        int maxSoFarLeft = INT_MIN;
+        int maxSoFarRight = INT_MIN;
+
+        // finding maxsofarleft value for each element
+
+        for(int i = 0 ; i < n ; i++) {
+            maxSoFarLeft = max(maxSoFarLeft, height[i]);
+            maxL[i] = maxSoFarLeft;
+        }
+
+        // finding maxsoforright value for each element
+
+        for(int i = n-1 ; i >= 0 ; i--) {
+            maxSoFarRight = max(maxSoFarRight, height[i]);
+            maxR[i] = maxSoFarRight;
+        }
+
+        // build a array -> get diff of maxL and maxR and minus height[i] for getting that how much water each height can consume
+
+        vector<int> water(n);
+        int ans = 0;
+
+        for(int i = 0 ; i < n ; i++) {
+            water[i] = (min(maxR[i], maxL[i])) - height[i];
+            ans += water[i];
+        }
+
+        return ans;
+    }
+};
