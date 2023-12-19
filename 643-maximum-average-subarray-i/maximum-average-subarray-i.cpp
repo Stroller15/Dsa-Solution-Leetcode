e2238bf9f64cd8c99e5172bb7c99c972985712bf
Shubham Verma <84174011+Stroller15@users.noreply.github.com>
2023-12-19 14:31:45 +0530

    Time: 138 ms (56.54%) | Memory: 109.9 MB (98.37%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -0,0 +1,42 @@
+// class Solution {
+// public:
+//     double findMaxAverage(vector<int>& nums, int k) {
+//         double maxAverage = INT_MIN;
+
+//         for(int i = 0 ; i <= nums.size() - k ; i++) {
+//             double avg = 0;
+//             for(int j = i ; j < i+k ; j++) {
+//                 avg += nums[j];
+//             }
+//             avg = avg/k;
+//             maxAverage = max(maxAverage, avg);
+//         }
+//         return maxAverage;
+//     }
+// };
+
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        int i = 0, j = 0;
+        double sum = 0, maxAverage = INT_MIN;
+
+        while(j < n) {
+            sum += nums[j];
+
+            if((j - i + 1) < k) j++;
+
+            else if((j - i + 1) == k) {
+                double avg = sum/k;
+                maxAverage = max(maxAverage, avg);
+
+                sum -= nums[i];
+                i++;
+                j++;
+            }
+        }
+        return maxAverage;        
+    }
+};
